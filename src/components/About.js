07c9713c2405ea34f324c9ed40fd330e8696c4d8
Kartik1745 <kartikpatil1997@gmail.com>
2023-02-12 16:38:45 -0500

    Creating About Section

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import './About.css';
+
+const About = () => {
+    return (
+        <div id='about' className='About'>
+            <h1 className='title'>HEY, I'M KARTIK PATIL</h1>
+            <div className='about-me'>
+                <p className='about-me-text'>
+                I am a full stack developer with expertise in creating visually appealing and
+                efficient web applications with a strong focus on user experience. 
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
