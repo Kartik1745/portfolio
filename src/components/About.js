0653dcaf5d75eac7665dfc91eac57bf588b2ba6a
Kartik1745 <kartikpatil1997@gmail.com>
2023-02-12 23:27:11 -0500

    Updating App and about components

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,23 @@ import './About.css';
 const About = () => {
     return (
         <div id='about' className='About'>
-            <h1 className='title'>HEY, I'M KARTIK PATIL</h1>
             <div className='about-me'>
-                <p className='about-me-text'>
-                I am a full stack developer with expertise in creating visually appealing and
-                efficient web applications with a strong focus on user experience. 
-                </p>
+                <div className='about-me-text'>
+                    <h1 className='abt-me'>ABOUT ME</h1>
+                    <p className='my-info'>
+                    Hi, I'm a <span id='wk-info'>full stack developer</span> with a passion for building dynamic and efficient 
+                    web applications. My expertise lies in developing and deploying end-to-end solutions, 
+                    from the front-end user interface to the back-end server-side logic. I have hands-on 
+                    experience in multiple programming languages and web technologies, including <span id='wk-info'>HTML, 
+                    CSS, JavaScript, React, Node.js.</span> 
+                    </p>
+                    <p>
+                    I strive to keep up with the latest developments in the field, and I am always 
+                    looking for new challenges to enhance my skills and bring innovative ideas to life.
+                    If you're looking for a <span id='wk-info'>talented</span> and <span id='wk-info'>motivated</span> software developer, 
+                    Feel free to Connect or Follow me on my <a id='lnkd-link' href='https://www.linkedin.com/in/kartikpatil98/' target="_blank">Linkedin</a>.
+                    </p>
+                </div>
             </div>
         </div>
     );
